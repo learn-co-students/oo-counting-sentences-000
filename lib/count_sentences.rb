75d07023793b6c390b17b3e44d207e8e23f819d9
mbrickenworthe <adamwinters456@gmail.com>
2015-07-10 19:13:43 -0400

    Changing the string

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,8 +1,17 @@
 # Write a method on String called `count_sentences` that returns the number of
 # sentences in the string it is called on
+require "pry"
 
 class String
   def count_sentences
-    # code goes here
+  array = self.split(/(\. )|(\? )|(\! )/)
+  if array.length == 0
+    return 0
+  elsif array.length.odd?
+    final = array.length + 1
+  else
   end
+  return final/2
+  end
+  
 end
