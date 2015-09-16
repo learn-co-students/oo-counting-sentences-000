69660a3df31750d6a4c0677cdfb9429b59e3cf0d
allensoberano <allensoberano@gmail.com>
2015-09-16 03:28:05 -0700

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,8 +1,14 @@
 # Write a method on String called `count_sentences` that returns the number of
 # sentences in the string it is called on
-
+require 'pry'
 class String
   def count_sentences
     # code goes here
+    sentence_array = []
+    sentence_array = self.split (/[\.\?\!]+/)
+    sentence_array.count
   end
 end
+
+# test = "This is a sentence. This one Too!! Even This... And Here."
+# test.count_sentences
